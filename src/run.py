058b55b68c95f5d17efa45f0ebd549f4ada4334f
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2025-08-31 17:14:28 -0400

    Change run.py

--- src/run.py
+++ src/run.py
@@ -2,11 +2,21 @@ import lib.ros_robot_controller_sdk as rrc
 import time
 import os
 
+MODE = "OpenChallenge"
+#MODE = "ObstacleChallenge"
+
+script_dir = os.path.dirname(__file__)
 board = rrc.Board()
 board.enable_reception()
 time.sleep(1)
-board.set_rgb([[1, 255, 255, 255], [2, 255, 255, 255]])
-board.set_buzzer(1700, 0.1, 0.6, 2)
+
+if MODE == "OpenChallenge":
+  board.set_rgb([[1, 255, 255, 255], [2, 255, 255, 255]])
+if MODE == "ObstacleChallenge":
+  board.set_rgb([[1, 0, 127, 0], [2, 127, 0, 0]])
+board.set_buzzer(1700, 0.2, 0.8, 2)
+time.sleep(2 * (0.2 + 0.8))
+board.set_buzzer(1900, 0.25, 0.8, 1)
 
 while True:
   data = board.get_button()
@@ -14,11 +24,16 @@ while True:
     time.sleep(0.03)
     continue
   button, event = data
-  if button == 1:
+  if button != 1: continue
+  if MODE == "OpenChallenge":
     board.set_rgb([[1, 127, 127, 127], [2, 127, 127, 127]])
-    board.set_buzzer(1900, 0.1, 0.45, 1)
-    os.execlp("python3", "python3", "OpenChallenge.py")
-  elif button == 2:
+    time.sleep(1)
+    board.set_buzzer(1900, 0.1, 0.7, 2)
+    board.set_rgb([[1, 0, 0, 127], [2, 0, 0, 127]])
+    os.execlp("python3", "python3", script_dir + "/OpenChallenge.py")
+  elif MODE == "ObstacleChallenge":
     board.set_rgb([[1, 255, 0, 0], [2, 0, 255, 0]])
-    board.set_buzzer(1900, 0.1, 0.3, 2)
-    os.execlp("python3", "python3", "ObstacleChallenge.py")
+    time.sleep(1)
+    board.set_buzzer(1900, 0.1, 0.7, 2)
+    board.set_rgb([[1, 127, 0, 0], [2, 0, 127, 0]])
+    os.execlp("python3", "python3", script_dir + "/ObstacleChallenge.py")
