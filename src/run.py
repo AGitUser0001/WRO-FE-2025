dbc368aeb4236b5cf07423e87248a9cbc8537e1a
AGitUser0001 <123198477+AGitUser0001@users.noreply.github.com>
2025-09-01 15:04:34 -0400

    Changed code

--- src/run.py
+++ src/run.py
@@ -8,12 +8,14 @@ MODE = "OpenChallenge"
 script_dir = os.path.dirname(__file__)
 board = rrc.Board()
 board.enable_reception()
-time.sleep(1)
 
 if MODE == "OpenChallenge":
-  board.set_rgb([[1, 255, 255, 255], [2, 255, 255, 255]])
+  board.set_rgb([[1, 0, 0, 63], [2, 0, 0, 63]])
 if MODE == "ObstacleChallenge":
-  board.set_rgb([[1, 0, 127, 0], [2, 127, 0, 0]])
+  board.set_rgb([[1, 63, 0, 0], [2, 0, 63, 0]])
+time.sleep(0.8)
+
+board.set_rgb([[1, 255, 255, 255], [2, 255, 255, 255]])
 board.set_buzzer(1700, 0.2, 0.8, 2)
 time.sleep(2 * (0.2 + 0.8))
 board.set_buzzer(1900, 0.25, 0.8, 1)
@@ -32,7 +34,7 @@ while True:
     board.set_rgb([[1, 0, 0, 127], [2, 0, 0, 127]])
     os.execlp("python3", "python3", script_dir + "/OpenChallenge.py")
   elif MODE == "ObstacleChallenge":
-    board.set_rgb([[1, 255, 0, 0], [2, 0, 255, 0]])
+    board.set_rgb([[1, 127, 127, 127], [2, 127, 127, 127]])
     time.sleep(1)
     board.set_buzzer(1900, 0.1, 0.7, 2)
     board.set_rgb([[1, 127, 0, 0], [2, 0, 127, 0]])
